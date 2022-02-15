c7ffa36927c8a4c78b64f3c5413bc7d4c9f6c98a
jahyun.shin <jahyunshin@gmail.com>
2022-02-15 13:25:36 +0900

    1. 접근 제어자 개선 및 반환 값 보완 2. rest api delete 추가

--- src/main/java/com/akadow/prototype001/controller/Rest.java
+++ src/main/java/com/akadow/prototype001/controller/Rest.java
@@ -1,5 +1,6 @@
 package com.akadow.prototype001.controller;
 
+import com.akadow.prototype001.service.DeleteService;
 import com.akadow.prototype001.service.DummyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,33 +10,51 @@ import java.util.Collections;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value="/rest")
+@RequestMapping(value = "/rest")
 public class Rest {
 
+    private DeleteService deleteService;
     private DummyService dummyService;
 
     @Autowired
-    void setDummyService(DummyService dummyService) {
+    private void setDeleteService(DeleteService deleteService) {
+        this.deleteService = deleteService;
+    }
+
+    @Autowired
+    private void setDummyService(DummyService dummyService) {
         this.dummyService = dummyService;
     }
 
+    private Map<String, String> notMatchedMap = Collections.singletonMap("key", "Not Matched.");
+
     @GetMapping("")
-    public ResponseEntity<String> first() {
+    private ResponseEntity<String> first() {
         return ResponseEntity.ok("## Called rest");
     }
 
     @GetMapping("custom/{command}")
-    public @ResponseBody ResponseEntity<Map<String, String>> customCommand(@PathVariable("command") String command) {
+    private @ResponseBody ResponseEntity<Map<String, String>> customCommand(@PathVariable("command") String command) {
         Map<String, String> returnMap;
         switch (command) {
             case "dummy":
                 returnMap = dummyService.createRest();
                 break;
             default:
-                returnMap = Collections.singletonMap("key", "Not Matched.");
+                returnMap = notMatchedMap;
         }
 
         return ResponseEntity.ok(returnMap);
+    }
+
+    @DeleteMapping("delete/{command}/{key}")
+    private Map<String, String> delete(@PathVariable("command") String command, @PathVariable("key") String key) {
+        switch (command) {
+            case "excludedChorusMap":
+                return deleteService.excludedChorusMap(key);
+            default:
+                return notMatchedMap;
+        }
     }
 
 }
